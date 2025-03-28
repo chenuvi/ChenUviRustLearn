2b20afd1fe40c8a13a9118058ed27b5b7925fc8d
kunchen16 <kunchen16@iflytek.com>
2025-03-28 17:29:10 +0800

    feat(hello): 返回 html

--- listings/ch21_web_server/src/main.rs
+++ listings/ch21_web_server/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 fn main() {
@@ -11,8 +12,10 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
+    // TODO: find file 
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n {}", contents);
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
