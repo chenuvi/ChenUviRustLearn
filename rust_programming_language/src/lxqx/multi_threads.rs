e490f8862bc8f3ed3c375e11212b585ae08bfd5d
chenuvi <chenuvi@outlook.com>
2025-05-20 23:58:14 +0800

    feat: multiple threads 2: parallel_calculate

--- rust_programming_language/src/lxqx/multi_threads.rs
+++ rust_programming_language/src/lxqx/multi_threads.rs
@@ -1,9 +1,11 @@
-use std::any;
+use std::sync::{Arc, Mutex};
 use std::time::Instant;
+use std::{any, thread};
 
 #[allow(dead_code)]
 pub fn run() {
     count_time(serialized_calculate);
+    count_time(parallel_calculate);
 }
 
 const TOTAL: u64 = 1000000000;
@@ -23,4 +25,30 @@ fn serialized_calculate() {
     println!("sum: {}", sum);
 }
 
-// fn parallel_calculate() {}
+fn parallel_calculate() {
+    let chunk_size = TOTAL / 16;
+    let sum = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for i in 0..16 {
+        let start = i * chunk_size;
+        let end = if i == 15 { TOTAL } else { chunk_size * (i + 1) };
+
+        let sum_clone = Arc::clone(&sum);
+        let handle = thread::spawn(move || {
+            let mut local_sum = 0;
+            for i in start..end {
+                local_sum += i;
+            }
+            let mut sum = sum_clone.lock().unwrap();
+            *sum += local_sum;
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("sum: {}", sum.lock().unwrap());
+}
